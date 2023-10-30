89b3f02de984c555f9dcbb396899965fc3fbc1fc
ych466 <94956955+ych466@users.noreply.github.com>
2023-10-30 14:04:46 +0800

    Delete Train.py

--- Train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo-cls/yolov8x-cls.pt")  #根据需求选择模型,见yolo官网
-if __name__ == '__main__':
-    model.train(data='dataset', epochs=100, batch=-1, imgsz=150) #根据需要设置参数
